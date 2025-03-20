22fc3fc2fe333274aa7fa921148aec27f7bc7ce4
AndriiMyhun <andrii.myhun@gmail.com>
2025-03-20 22:26:41 +0100

    1.0.0.1
    verification code via email
    validation password
    token JWT

--- app/src/main/java/com/example/smarthome/ui/help/HelpFragment.kt
+++ app/src/main/java/com/example/smarthome/ui/help/HelpFragment.kt
@@ -1,10 +1,18 @@
 package com.example.smarthome.ui.help
 
+import android.content.ActivityNotFoundException
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.fragment.findNavController
 import com.example.smarthome.R
 import com.example.smarthome.databinding.FragmentHelpBinding
@@ -18,15 +26,38 @@ class HelpFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+        savedInstanceState: Bundle?): View {
+
         _binding = FragmentHelpBinding.inflate(inflater, container, false)
 
-
-        binding.btnBackHelp.setOnClickListener {
+        binding.btnBackHelpActivity.setOnClickListener {
 
             findNavController().navigateUp()
         }
+
+        binding.textPhoneHelpActivity.setOnClickListener {
+            val phoneNumber = "tel:+48573870821"
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse(phoneNumber)
+            }
+            startActivity(intent)
+        }
+
+        binding.textEmailHelpActivity.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:etiap22@gmail.com")
+                putExtra(Intent.EXTRA_SUBJECT, "Subject.")
+                putExtra(Intent.EXTRA_TEXT, "Describe a problem.")
+            }
+            emailIntent.setPackage("com.google.android.gm")
+
+            try {
+                startActivity(emailIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(requireContext(), "Gmail not found.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
         return binding.root
     }
